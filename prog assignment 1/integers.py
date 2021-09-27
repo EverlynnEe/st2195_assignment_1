1f5668a6d97f490a6032759e557df9d84ab0b7a0
EverlynnEe <91049309+EverlynnEe@users.noreply.github.com>
2021-09-27 22:45:57 +0800

    Add files via upload

---
+++ prog assignment 1/integers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Print integers
+# ## Print from 1 to 10
+# **using a loop **
+# 
+
+# In[2]:
+
+
+for i in range(1,11):
+    print(i)
+
+
+# In[ ]:
+
+
+
+
---
+++ prog assignment 1/spyder.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 22:28:23 2021
+
+@author: yuunl
+"""
+
+## Practice assignment 1
+#%%
+for i in range(1,11):
+    print(i)
+#%%
